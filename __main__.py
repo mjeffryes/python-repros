22ac8b99885664743023814a6dbdc1d114db8a5e
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-07-28 14:03:10 -0700

    Repro case for aws-native issue #846

--- __main__.py
+++ __main__.py
@@ -1,3 +1,27 @@
 """A Python Pulumi program"""
 
 import pulumi
+import pulumi_aws_native as aws_native
+
+aws_native.dynamodb.Table(
+       "abc",
+       table_name="example",
+       billing_mode="PROVISIONED",
+       opts=pulumi.ResourceOptions(protect=True),
+       attribute_definitions=[
+           aws_native.dynamodb.TableAttributeDefinitionArgs(
+               attribute_name="PrimaryKey",
+               attribute_type="S",
+            )
+        ],
+        provisioned_throughput=aws_native.dynamodb.TableProvisionedThroughputArgs(
+            read_capacity_units=100,  
+            write_capacity_units=1,
+        ),
+        key_schema=[
+            aws_native.dynamodb.TableKeySchemaArgs(
+                attribute_name="PrimaryKey",
+                key_type="HASH",
+             ),
+        ],
+)
