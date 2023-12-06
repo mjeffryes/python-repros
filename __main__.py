add1b0d478de790f1b5ea197540d5e9a48aef1da
Matthew Jeffryes <mjeffryes@pulumi.com>
2023-12-06 05:48:59 -0800

    No repro

--- __main__.py
+++ __main__.py
@@ -1,3 +1,39 @@
 """A Python Pulumi program"""
 
 import pulumi
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
+from pulumi_command.local import Command
+
+app_name = "nginx"
+app_labels = { "app": app_name }
+
+deployment = Deployment(
+    app_name,
+    spec={
+        "selector": { "match_labels": app_labels },
+        "replicas": 1,
+        "template": {
+            "metadata": { "labels": app_labels },
+            "spec": { "containers": [{ "name": app_name, "image": "nginx" }] }
+        }
+    })
+
+# Allocate an IP to the Deployment.
+service = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    })
+
+echo = Command(
+    "echo", 
+    create = service.status.apply( lambda s: "echo " + s.load_balancer.ingress[0]["ip"] ),
+);
+
+pulumi.export("ip", echo.stdout);
